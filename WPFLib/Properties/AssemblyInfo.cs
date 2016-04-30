c2cbbff045025d97571310f2062849174b30cea8
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2016-04-30 11:18:14 +0200

    Updated AssemblyInfo with XML namespaces

--- WPFLib/Properties/AssemblyInfo.cs
+++ WPFLib/Properties/AssemblyInfo.cs
@@ -1,18 +1,17 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WPFLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("WPF development libary written by webmaster442. This is free software licensed under the MIT license")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Webmaster442")]
 [assembly: AssemblyProduct("WPFLib")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Webmaster442  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,13 +30,13 @@ using System.Windows;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
-[assembly:ThemeInfo(
+[assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                             //(used if a resource is not found in the page, 
-                             // or application resource dictionaries)
+                                     //(used if a resource is not found in the page, 
+                                     // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                      //(used if a resource is not found in the page, 
-                                      // app, or any theme specific resource dictionaries)
+                                              //(used if a resource is not found in the page, 
+                                              // app, or any theme specific resource dictionaries)
 )]
 
 
@@ -53,3 +52,9 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+//XAML NAMESPACE DEFS
+[assembly: XmlnsPrefix("http://webmaster442.hu/wpflib", "WPFLib.Controls")]
+[assembly: XmlnsPrefix("http://webmaster442.hu/wpflib", "WPFLib.Converters")]
+[assembly: XmlnsPrefix("http://webmaster442.hu/wpflib", "WPFLib.Dialogs")]
+[assembly: XmlnsPrefix("http://webmaster442.hu/wpflib", "WPFLib.Extensions")]
