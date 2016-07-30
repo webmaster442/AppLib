2c1f41b11f8142c41dd337365d7f6652e5051408
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2016-07-30 11:43:29 +0200

    AnimatedTabControl added

---
+++ WPFLib/Controls/AnimatedTabControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFLib.Controls
+{
+    /// <summary>
+    /// A tab control with pixel shader transition effects
+    /// </summary>
+    public class AnimatedTabControl: TabControl
+    {
+        static AnimatedTabControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnimatedTabControl), new FrameworkPropertyMetadata(typeof(AnimatedTabControl)));
+        }
+    }
+}
