df20e5579360d8ff9aecadb5ac4b594b0df93bfa
webmaster442 <ruzsinszki.gabor@gmail.com>
2017-01-03 22:21:33 +0100

    ScreenHelper functionality extended

--- AppLib.WPF/ScreenHelper.cs
+++ AppLib.WPF/ScreenHelper.cs
@@ -2,7 +2,7 @@
 using System.Windows;
 using System.Windows.Interop;
 
-namespace WPFLib
+namespace AppLib.WPF
 {
     /// <summary>
     /// A small, screen size helper class
@@ -10,24 +10,33 @@ namespace WPFLib
     public static class ScreenHelper
     {
         private static readonly List<Size> _screens;
+        private static readonly List<Point> _screenstartpoints;
 
         static ScreenHelper()
         {
             var screens = System.Windows.Forms.Screen.AllScreens;
             _screens = new List<Size>(screens.Length);
+            _screenstartpoints = new List<Point>(screens.Length);
             foreach (var screen in screens)
             {
                 var s = new Size(screen.Bounds.Width, screen.Bounds.Height);
+                var p = new Point(screen.Bounds.Left, screen.Bounds.Top);
                 _screens.Add(s);
+                _screenstartpoints.Add(p);
             }
         }
 
         /// <summary>
         /// Gets the current monitor sizes
         /// </summary>
-        public static Size[] ScreenSizes
+        public static Size[] ScreenSizes => _screens.ToArray();
+        
+        /// <summary>
+        /// Gets the count of available screens
+        /// </summary>
+        public static int Count
         {
-            get { return _screens.ToArray(); }
+            get { return _screens.Count; }
         }
 
         /// <summary>
@@ -48,12 +57,42 @@ namespace WPFLib
         /// <param name="w">Window to move</param>
         public static void MoveToDefaultScreen(Window w)
         {
-            var def = ScreenSizes[0];
-            w.WindowState = WindowState.Normal;
-            w.Left = (def.Width - w.Width) / 2;
-            w.Top = (def.Height - w.Height) / 2;
+            MoveToScreen(w, 0);
+        }
+
+        /// <summary>
+        /// Moves the window to the specified screen
+        /// </summary>
+        /// <param name="index">screen index</param>
+        public static void MoveToScreen(Window w, int index)
+        {
+            var s = _screens[index];
+            var p = _screenstartpoints[index];
+
+            var x = p.X + ((s.Width - w.Width) / 2);
+            var y = p.Y + ((s.Height - w.Height) / 2);
+            w.Left = x;
+            w.Top = y;
             w.BringIntoView();
-            w.Activate(); 
+            w.Activate();
+
+        }
+
+        /// <summary>
+        /// Maximizes window on selected screen
+        /// </summary>
+        /// <param name="w">Window to maximize</param>
+        /// <param name="number">Screen number</param>
+        public static void MaximizeOnScreen(Window w, int index)
+        {
+            var s = _screens[index];
+            var p = _screenstartpoints[index];
+            w.Left = p.X;
+            w.Top = p.Y;
+            w.Width = s.Width;
+            w.Height = s.Height;
+            w.BringIntoView();
+            w.Activate();
         }
     }
 }
