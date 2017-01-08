99e556fce6d6b8c14f9165358c057804e067544b
webmaster442 <ruzsinszki.gabor@gmail.com>
2017-01-08 18:00:47 +0100

    Added a few extension methods

--- AppLib.Common/Extensions/FloatExtensions.cs
+++ AppLib.Common/Extensions/FloatExtensions.cs
@@ -1,23 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AppLib.Common.Extensions
 {
     /// <summary>
-    /// Double number extension methods
+    /// Float number extension methods
     /// </summary>
-    public static class DoubleExtensions
+    public static class FloatExtensions
     {
         /// <summary>
-        /// Compare two doubles with a maximum allowed difference.
+        /// Compare two floats with a maximum allowed difference.
         /// If differnece is not set, then by default 0.00001 is aplied
         /// </summary>
-        /// <param name="d">double number</param>
-        /// <param name="other">other double number</param>
+        /// <param name="d">float number</param>
+        /// <param name="other">other float number</param>
         /// <param name="diff">maximum allowed difference.</param>
         /// <returns></returns>
-        public static bool EqualsWithTolerance(this double d, double other, double diff = 0.00001)
+        public static bool EqualsWithTolerance(this float d, float other, float diff = 0.00001f)
         {
-            double sub = 0;
+            float sub = 0;
             if (other > d) sub = other - d;
             else sub = d - other;
             return sub < diff;
@@ -26,11 +30,11 @@ namespace AppLib.Common.Extensions
         /// <summary>
         /// Linear interpolation between two values
         /// </summary>
-        /// <param name="d">double number</param>
+        /// <param name="d">float number</param>
         /// <param name="other">other number</param>
         /// <param name="amount">amount of interpolation. Must be berween 0 and 1</param>
         /// <returns>a value between the first parameter and the second parameter based on the amount</returns>
-        public static double Lerp(this double d, double other, double amount)
+        public static float Lerp(this float d, float other, float amount)
         {
             if (amount < 0 || amount > 1)
                 throw new ArgumentException("amount must be between 0 and 1");
@@ -46,7 +50,7 @@ namespace AppLib.Common.Extensions
         /// <param name="out_min">the lower bound of the value's target range</param>
         /// <param name="out_max">the upper bound of the value's target range</param>
         /// <returns></returns>
-        public static double Map(this double x, double in_min, double in_max, double out_min, double out_max)
+        public static float Map(this float x, float in_min, float in_max, float out_min, float out_max)
         {
             return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
         }
---
+++ AppLib.WPF/Extensions/ColorExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using AppLib.Common.Extensions;
+
+namespace AppLib.WPF.Extensions
+{
+    /// <summary>
+    /// Extension methods for the color type
+    /// </summary>
+    public static class ColorExtensions
+    {
+        /// <summary>
+        /// Fade a color to another color
+        /// </summary>
+        /// <param name="source">Color Source</param>
+        /// <param name="target">Target color</param>
+        /// <param name="amount">Amount of fading. Must be between 0 and 1</param>
+        /// <returns>A color faded to another color</returns>
+        public static Color FadeTo(this Color source, Color target, double amount)
+        {
+            double r = source.R;
+            double g = source.G;
+            double b = source.B;
+            r = r.Lerp(target.R, amount);
+            g = g.Lerp(target.G, amount);
+            b = b.Lerp(target.B, amount);
+            return Color.FromRgb((byte)r, (byte)g, (byte)b);
+        }
+
+        /// <summary>
+        /// Computes the negative color corresponding to the color. Ignores the alpha channel
+        /// </summary>
+        /// <param name="c">a color</param>
+        /// <returns>the color's negative color</returns>
+        public static Color Negative(this Color c)
+        {
+            return Color.FromRgb((byte)(255 - c.R), (byte)(255 - c.G), (byte)(255 - c.B));
+        }
+    }
+}
