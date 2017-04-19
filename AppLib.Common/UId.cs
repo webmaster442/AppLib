bf2c431b0bae10fcf71a62720a5411fb00f6263d
webmaster442 <ruzsinszki.gabor@gmail.com>
2017-04-19 15:45:28 +0200

    UID & ExceptioExtensions

---
+++ AppLib.Common/UId.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AppLib.Common
+{
+    /// <summary>
+    /// A Variable length Unique identifier, similar to GUID
+    /// </summary>
+    public sealed class UId : IEquatable<UId>, IComparable<UId>
+    {
+        private byte[] _data;
+
+        private UId(byte[] data)
+        {
+            _data = data;
+        }
+
+        /// <summary>
+        /// Creates a new UID from a long value.
+        /// The Resulting UID will be 8 bytes
+        /// </summary>
+        /// <param name="value">A value</param>
+        /// <returns>A Uid containing the value</returns>
+        public static UId Create(long value)
+        {
+            return new UId(BitConverter.GetBytes(value));
+        }
+
+        /// <summary>
+        /// Creates a new UID from an unsigned long value.
+        /// The Resulting UID will be 8 bytes
+        /// </summary>
+        /// <param name="value">A value</param>
+        /// <returns>A Uid containing the value</returns>
+        public static UId Create(ulong value)
+        {
+            return new UId(BitConverter.GetBytes(value));
+        }
+
+        /// <summary>
+        /// Creates a new random UID 
+        /// </summary>
+        /// <param name="size">Size of UID in bytes. By default 8 is used</param>
+        /// <returns>A new random UID</returns>
+        public static UId Create(int size = 8)
+        {
+            var buffer = new byte[size];
+            var rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(buffer);
+            return new UId(buffer);
+        }
+
+        /// <summary>
+        /// Gets the UID length
+        /// </summary>
+        public int Length
+        {
+            get { return _data.Length; }
+        }
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            int counter = 0;
+            foreach (var b in _data)
+            {
+                sb.AppendFormat("{0:X2}", b);
+                if (counter % 2 == 0) sb.Append("-");
+                ++counter;
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns>true if the specified object is equal to the current object; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            var other = obj as UId;
+            if (other == null) return false;
+            else return Equals(this, other);
+        }
+
+        /// <summary>
+        /// Computes the hash of the current UID
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                for (int i = 0; i < _data.Length; i++)
+                {
+                    hash = hash * 23 + _data[i].GetHashCode();
+                }
+                return hash;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="other">The object to compare with the current object.</param>
+        /// <returns>true if the specified object is equal to the current object; otherwise, false.</returns>
+        public bool Equals(UId other)
+        {
+            if (this.Length != other.Length) return false;
+            for (int i = 0; i < other._data.Length; i++)
+            {
+                if (this._data[i] != other._data[i])
+                    return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Compares the current instance with another object of the same type and returns an integer that indicates whether the current instance precedes, follows, or occurs in the same position in the sort order as the other object.
+        /// </summary>
+        /// <param name="other">An object to compare with this instance.</param>
+        /// <returns>A value that indicates the relative order of the objects being compared.</returns>
+        public int CompareTo(UId other)
+        {
+            if (Length > other.Length) return -1;
+            for (int i = Length - 1; i >= 0; i--)
+            {
+                if (_data[i] > other._data[i]) return -1;
+                else if (_data[i] < other._data[i]) return 1;
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Compares two UID objects equality
+        /// </summary>
+        /// <param name="One">First UID</param>
+        /// <param name="other">Second UID</param>
+        /// <returns>true, if the two instances are identical, false otherwise</returns>
+        public static bool operator == (UId One, UId other)
+        {
+            return One.Equals(other);
+        }
+
+        /// <summary>
+        /// Compares two UID objects not equality
+        /// </summary>
+        /// <param name="One">First UID</param>
+        /// <param name="other">Second UID</param>
+        /// <returns>true, if the two instances are not identical, false otherwise</returns>
+        public static bool operator !=(UId One, UId other)
+        {
+            return !One.Equals(other);
+        }
+    }
+}
