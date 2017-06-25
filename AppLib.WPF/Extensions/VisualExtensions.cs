4031b7f67263d9b86f483c6fb75e97f128f78353
webmaster442 <ruzsinszki.gabor@gmail.com>
2017-06-25 10:56:10 +0200

    Visual Extensions

---
+++ AppLib.WPF/Extensions/VisualExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace AppLib.WPF.Extensions
+{
+    /// <summary>
+    /// Visual class extensions
+    /// </summary>
+    public static class VisualExtensions
+    {
+        /// <summary>
+        /// Get Visual Descendant casted to type
+        /// </summary>
+        /// <param name="element">Element that Descendant needs to be found</param>
+        /// <param name="Descendant">Descendant to find</param>
+        /// <returns>Descendant visual</returns>
+        public static T GetDescendantByType<T>(this Visual element, Type Descendant = null) where T: Visual
+        {
+            if (element == null) return null;
+
+            if (Descendant == null) Descendant = typeof(T);
+
+            if (element.GetType() == Descendant) return (T)element;
+            T foundElement = null;
+            if (element is FrameworkElement)
+            {
+                (element as FrameworkElement).ApplyTemplate();
+            }
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+            {
+                Visual visual = VisualTreeHelper.GetChild(element, i) as Visual;
+                foundElement = GetDescendantByType<T>(visual, Descendant);
+                if (foundElement != null)
+                    break;
+            }
+            return (T)foundElement;
+        }
+    }
+}
