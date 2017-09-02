24752f60cac4d7eeb118358b14f08467e89517a4
webmaster442 <ruzsinszki.gabor@gmail.com>
2017-09-02 17:53:42 +0200

    Translation markup extension

---
+++ AppLib.WPF/Transalate/LanguageChangedEventManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows;
+
+namespace AppLib.WPF.Transalate
+{
+    /// <summary>
+    /// Manager for Language change events
+    /// </summary>
+    public class LanguageChangedEventManager : WeakEventManager
+    {
+        /// <summary>
+        /// Add a Listener
+        /// </summary>
+        /// <param name="source">Listener source</param>
+        /// <param name="listener">Listener to add</param>
+        public static void AddListener(TranslationManager source, IWeakEventListener listener)
+        {
+            CurrentManager.ProtectedAddListener(source, listener);
+        }
+
+        /// <summary>
+        /// Remove listener
+        /// </summary>
+        /// <param name="source">Listener source</param>
+        /// <param name="listener">Listener to remove</param>
+        public static void RemoveListener(TranslationManager source, IWeakEventListener listener)
+        {
+            CurrentManager.ProtectedRemoveListener(source, listener);
+        }
+
+        private void OnLanguageChanged(object sender, EventArgs e)
+        {
+            DeliverEvent(sender, e);
+        }
+
+        /// <summary>
+        /// Start listening process
+        /// </summary>
+        /// <param name="source">source of the listening</param>
+        protected override void StartListening(object source)
+        {
+            var manager = (TranslationManager)source;
+            manager.LanguageChanged += OnLanguageChanged;
+        }
+
+        /// <summary>
+        /// Stop listening 
+        /// </summary>
+        /// <param name="source">source of the listening</param>
+        protected override void StopListening(object source)
+        {
+            var manager = (TranslationManager)source;
+            manager.LanguageChanged -= OnLanguageChanged;
+        }
+
+        /// <summary>
+        /// Current Manager instance
+        /// </summary>
+        private static LanguageChangedEventManager CurrentManager
+        {
+            get
+            {
+                Type managerType = typeof(LanguageChangedEventManager);
+                var manager = (LanguageChangedEventManager)GetCurrentManager(managerType);
+                if (manager == null)
+                {
+                    manager = new LanguageChangedEventManager();
+                    SetCurrentManager(managerType, manager);
+                }
+                return manager;
+            }
+        }
+
+    }
+}
---
+++ AppLib.WPF/Transalate/ResxTranslationProvider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+
+namespace AppLib.WPF.Transalate
+{
+    public class ResxTranslationProvider : ITranslationProvider
+    {
+        private readonly ResourceManager _resourceManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResxTranslationProvider"/> class.
+        /// </summary>
+        /// <param name="baseName">Name of the base.</param>
+        /// <param name="assembly">The assembly.</param>
+        public ResxTranslationProvider(string baseName, Assembly assembly)
+        {
+            _resourceManager = new ResourceManager(baseName, assembly);
+        }
+
+        /// <summary>
+        /// Get available languages
+        /// </summary>
+        public IEnumerable<CultureInfo> Languages
+        {
+            get
+            {
+                //Get all culture 
+                CultureInfo[] culture = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+                //Find the location where application installed.
+                string exeLocation = Path.GetDirectoryName(Uri.UnescapeDataString(new UriBuilder(Assembly.GetExecutingAssembly().CodeBase).Path));
+
+                //Return all culture for which satellite folder found with culture code.
+                return culture.Where(cultureInfo => Directory.Exists(Path.Combine(exeLocation, cultureInfo.Name)));
+            }
+        }
+
+        /// <summary>
+        /// See <see cref="ITranslationProvider.Translate" />
+        /// </summary>
+        public object Translate(string key)
+        {
+            return _resourceManager.GetString(key);
+        }
+    }
+}
