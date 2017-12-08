2d099c82626cf25b4b6d42e429970323799b3631
webmaster442 <ruzsinszki.gabor@gmail.com>
2017-12-08 22:29:45 +0100

    Commands rework

---
+++ AppLib.MVVM/Commands/DelegateCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AppLib.MVVM
+{
+    /// <summary>
+    /// An object based delegate command
+    /// </summary>
+    public class DelegateCommand : DelegateCommand<object>
+    {
+        /// <summary>
+        /// Creates a new instance of DelegateCommand
+        /// </summary>
+        /// <param name="action">Action to perform</param>
+        public DelegateCommand(Action<object> action) : base(action)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of DelegateCommand
+        /// </summary>
+        /// <param name="action">Action to perform</param>
+        /// <param name="canExecute">Can Execute function</param>
+        public DelegateCommand(Action<object> action, Predicate<object> canExecute) : base(action, canExecute)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of DelegateCommand
+        /// </summary>
+        /// <param name="action">Action to execute</param>
+        /// <param name="canExecute">Can Execute function</param>
+        /// <param name="state">Update binding on execute state</param>
+        public DelegateCommand(Action<object> action, Predicate<object> canExecute, UpdateBindingOnExecute state) : base(action, canExecute, state)
+        {
+        }
+    }
+}
