49e1dccf3b6647a746875d17421d69d9c4f4059f
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-01-13 16:33:13 +0100

    Set and set functions

---
+++ AppLib.Math/Set.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLib.Maths
+{
+    /// <summary>
+    /// A Set of numbers
+    /// </summary>
+    public class Set : List<double>
+    {
+        /// <summary>
+        /// Create a set from a list of numbers
+        /// </summary>
+        /// <param name="numbers">list of numbers</param>
+        public Set(params double[] numbers) : base(numbers.Length)
+        {
+            AddRange(numbers);
+        }
+
+
+        /// <summary>
+        /// Create a set from a list of numbers
+        /// </summary>
+        /// <param name="itms">list of numbers</param>
+        public Set(List<double> itms) : base(itms) { }
+
+        /// <summary>
+        /// Create a set
+        /// </summary>
+        /// <param name="count">Number of items to hold</param>
+        public Set(int count) : base(count) { }
+
+        /// <summary>
+        /// Add a number to every item in the set
+        /// </summary>
+        /// <param name="input">Input set</param>
+        /// <param name="number">number to add</param>
+        /// <returns>set with added items</returns>
+        public static Set operator +(Set input, double number)
+        {
+            var copy = new Set(input.Count);
+            Parallel.For(0, input.Count, i =>
+            {
+                copy[i] = input[i] + number;
+            });
+            return copy;
+        }
+
+        /// <summary>
+        /// Add a set's values to a nother set
+        /// </summary>
+        /// <param name="a">Input set 1</param>
+        /// <param name="b">Input set 2</param>
+        /// <returns>the sum of two sets</returns>
+        public static Set operator +(Set a, Set b)
+        {
+            var min = System.Math.Min(a.Count, b.Count);
+            var result = new Set(min);
+            Parallel.For(0, min, i =>
+            {
+                result[i] = a[i] + b[i];
+            });
+            return result;
+        }
+
+        /// <summary>
+        /// Subtract a number from a set's values
+        /// </summary>
+        /// <param name="input">input set</param>
+        /// <param name="number">number to subtract</param>
+        /// <returns>set after subtraction</returns>
+        public static Set operator -(Set input, double number)
+        {
+            var copy = new Set(input.Count);
+            Parallel.For(0, input.Count, i =>
+            {
+                copy[i] = input[i] + number;
+            });
+            return copy;
+        }
+
+        /// <summary>
+        /// Subtract a set's values from a nother set's values
+        /// </summary>
+        /// <param name="a">Set to subtract from</param>
+        /// <param name="b">Set to subtract</param>
+        /// <returns>set after subtraction</returns>
+        public static Set operator -(Set a, Set b)
+        {
+            var min = System.Math.Min(a.Count, b.Count);
+            var result = new Set(min);
+            Parallel.For(0, min, i =>
+            {
+                result[i] = a[i] - b[i];
+            });
+            return result;
+        }
+
+        /// <summary>
+        /// Multiply a set's every item with a number
+        /// </summary>
+        /// <param name="input">input set</param>
+        /// <param name="number">number to multiply with</param>
+        /// <returns>set after multiplication</returns>
+        public static Set operator *(Set input, double number)
+        {
+            var copy = new Set(input.Count);
+            Parallel.For(0, input.Count, i =>
+            {
+                copy[i] = input[i] * number;
+            });
+            return copy;
+        }
+
+        /// <summary>
+        /// Multiply a set's values from a nother set's values
+        /// </summary>
+        /// <param name="a">Set to multiply</param>
+        /// <param name="b">Set to multiply with</param>
+        /// <returns>set after multiplication</returns>
+        public static Set operator *(Set a, Set b)
+        {
+            var min = System.Math.Min(a.Count, b.Count);
+            var result = new Set(min);
+            Parallel.For(0, min, i =>
+            {
+                result[i] = a[i] * b[i];
+            });
+            return result;
+        }
+
+        /// <summary>
+        /// Divide a set's values with a number
+        /// </summary>
+        /// <param name="input">Set to divide</param>
+        /// <param name="number">Divisor</param>
+        /// <returns>Set after division</returns>
+        public static Set operator /(Set input, double number)
+        {
+            var copy = new Set(input.Count);
+            Parallel.For(0, input.Count, i =>
+            {
+                copy[i] = input[i] + number;
+            });
+            return copy;
+        }
+
+        /// <summary>
+        /// Divide a set's value's with a nother set's values
+        /// </summary>
+        /// <param name="a">Set to divide</param>
+        /// <param name="b">Divisior set</param>
+        /// <returns>Set after division</returns>
+        public static Set operator /(Set a, Set b)
+        {
+            var min = System.Math.Min(a.Count, b.Count);
+            var result = new Set(min);
+            Parallel.For(0, min, i =>
+            {
+                result[i] = a[i] / b[i];
+            });
+            return result;
+        }
+
+        /// <summary>
+        /// MOD a set's value's with a number
+        /// </summary>
+        /// <param name="input">Set to mod</param>
+        /// <param name="number">mod</param>
+        /// <returns>set after mod</returns>
+        public static Set operator %(Set input, double number)
+        {
+            var copy = new Set(input.Count);
+            Parallel.For(0, input.Count, i =>
+            {
+                copy[i] = input[i] % number;
+            });
+            return copy;
+        }
+
+        /// <summary>
+        ///  MOD a set's value's with a nother set's values
+        /// </summary>
+        /// <param name="a">Set to mod</param>
+        /// <param name="b">MOD set</param>
+        /// <returns>set after mod</returns>
+        public static Set operator %(Set a, Set b)
+        {
+            var min = System.Math.Min(a.Count, b.Count);
+            var result = new Set(min);
+            Parallel.For(0, min, i =>
+            {
+                result[i] = a[i] % b[i];
+            });
+            return result;
+        }
+
+        /// <summary>
+        /// Convert a set to string
+        /// </summary>
+        /// <returns>string representation of set</returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            int limit = Count;
+            bool fulllist = true;
+            if (Count > 12)
+            {
+                limit = 12;
+                fulllist = false;
+            }
+            for (int i = 0; i < limit; i++)
+            {
+                sb.AppendFormat("{0}, ", this[i]);
+            }
+            if (!fulllist) sb.Append("...");
+            return sb.ToString();
+
+        }
+    }
+}
--- AppLib.Math/TypeFunctions.cs
+++ AppLib.Math/TypeFunctions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Numerics;
 
 namespace AppLib.Maths
@@ -62,6 +63,147 @@ namespace AppLib.Maths
         public static Fraction Fraction(double numerator, double denominator)
         {
             return new Fraction(Convert.ToInt64(numerator), Convert.ToInt64(denominator));
+        }
+
+        /// <summary>
+        /// Create a set from a list of numbers
+        /// </summary>
+        /// <param name="d">set of numbers</param>
+        /// <returns>Number set</returns>
+        public static Set Set(params double[] d)
+        {
+            return new Set(d);
+        }
+
+        /// <summary>
+        /// Return the distint values from a set
+        /// </summary>
+        /// <param name="arg">Input set</param>
+        /// <returns>Set with distint values</returns>
+        public static Set Distinct(Set arg)
+        {
+            var res = arg.Distinct().ToList();
+            return new Set(res);
+        }
+
+        /// <summary>
+        /// Return the intersection of two sets
+        /// </summary>
+        /// <param name="arg1">set 1</param>
+        /// <param name="arg2">set 2</param>
+        /// <returns>Intersection of sets</returns>
+        public static Set Intersect(Set arg1, Set arg2)
+        {
+            var res = arg1.Intersect(arg2).ToList();
+            return new Set(res);
+        }
+
+        /// <summary>
+        /// Union of sets
+        /// </summary>
+        /// <param name="arg1">set 1</param>
+        /// <param name="arg2">set 2</param>
+        /// <returns>Union of inputs</returns>
+        public static Set Union(Set arg1, Set arg2)
+        {
+            var res = arg1.Union(arg2).ToList();
+            return new Set(res);
+        }
+
+        /// <summary>
+        /// Except a set from a nother set
+        /// </summary>
+        /// <param name="arg1">set 1</param>
+        /// <param name="arg2">set 2</param>
+        /// <returns>Excepted value</returns>
+        public static Set Except(Set arg1, Set arg2)
+        {
+            var res = arg1.Except(arg2).ToList();
+            return new Set(res);
+        }
+
+        /// <summary>
+        /// Create a set based on a series
+        /// </summary>
+        /// <param name="start">starting value</param>
+        /// <param name="d">difference</param>
+        /// <param name="items">number of items</param>
+        /// <returns>A set containing the series</returns>
+        public static Set Series(double start, double d, double items)
+        {
+            var set = new Set();
+            set.Add(start);
+            var current = start;
+            for (int i = 0; i < items; i++)
+            {
+                current += d;
+                set.Add(current);
+            }
+            return set;
+        }
+
+        /// <summary>
+        /// Create a set based on a geometric series
+        /// </summary>
+        /// <param name="start">start value</param>
+        /// <param name="q">difference</param>
+        /// <param name="items">number of items to create</param>
+        /// <returns>A set containing the series</returns>
+        public static Set GeometricSeries(double start, double q, double items)
+        {
+            var set = new Set(items);
+            set.Add(start);
+            var current = start;
+            for (int i = 0; i < items; i++)
+            {
+                current *= q;
+                set.Add(current);
+            }
+            return set;
+        }
+
+        /// <summary>
+        /// Create a set with random values
+        /// </summary>
+        /// <param name="min">minimum value</param>
+        /// <param name="max">maximum value</param>
+        /// <param name="items">number of items</param>
+        /// <returns>set of random items</returns>
+        public static Set RandomSet(double min, double max, double items)
+        {
+            var set = new Set(items);
+            Random r = new Random();
+            var imin = Convert.ToInt32(min);
+            var imax = Convert.ToInt32(max);
+            for (int i = 0; i < items; i++)
+            {
+
+                var item = r.Next(imin, imax);
+                set.Add(item);
+            }
+            return set;
+        }
+
+        /// <summary>
+        /// Sort a set ascending
+        /// </summary>
+        /// <param name="set">set to sort</param>
+        /// <returns>set in ascending order</returns>
+        public static Set SortA(Set set)
+        {
+            var q = from i in set orderby i ascending select i;
+            return new Set(q.ToList());
+        }
+
+        /// <summary>
+        /// Sort a set descending
+        /// </summary>
+        /// <param name="set">set to sort</param>
+        /// <returns>set in descending order</returns>
+        public static Set SortD(Set set)
+        {
+            var q = from i in set orderby i descending select i;
+            return new Set(q.ToList());
         }
     }
 }
