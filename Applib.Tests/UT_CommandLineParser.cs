e3c93860f9e41500c8229529949bdf6f7433a29d
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-02-02 11:23:47 +0100

    Tests rework

---
+++ Applib.Tests/UT_CommandLineParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppLib.Common.Console;
+
+namespace AppLib.Tests
+{
+    [TestClass]
+    public class UT_CommandLineParser
+    {
+        [TestMethod]
+        [DataRow("asd.exe test.file", 1, 0, 0)]
+        public void CommandLineParserTests(string rawCommand, int FilesCount, int switchCount, int switchWithArgumentCount)
+        {
+            Command
+        }
+    }
+}
