7b8211f4377ce313cc5ec532ec9526ab4ba3e2ec
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-02-28 20:33:49 +0100

    Controls change

---
+++ AppLib.WPF/Controls/Panels/RadialPanel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AppLib.WPF.Controls
+{
+    /// <summary>
+    /// A Panel for arranging items in a circle
+    /// </summary>
+    public class RadialPanel : Panel
+    {
+        /// <inhreritdoc/>
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            foreach (UIElement elem in Children)
+            {
+                //Give Infinite size as the avaiable size for all the children
+                elem.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+            }
+            return base.MeasureOverride(availableSize);
+        }
+
+
+        /// <inhreritdoc/>
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            if (Children.Count == 0) return finalSize;
+
+            double _angle = 0;
+            //Degrees converted to Radian by multiplying with PI/180
+            double _incrementalAngularSpace = (360.0 / Children.Count) * (Math.PI / 180);
+            //An approximate radii based on the avialable size , obviusly a better approach is needed here.
+            double radiusX = finalSize.Width / 2.4;
+            double radiusY = finalSize.Height / 2.4;
+
+            foreach (UIElement elem in Children)
+            {
+                //Calculate the point on the circle for the element
+                Point childPoint = new Point(Math.Cos(_angle) * radiusX, -Math.Sin(_angle) * radiusY);
+                //Offsetting the point to the Avalable rectangular area which is FinalSize.
+                Point actualChildPoint = new Point(finalSize.Width / 2 + childPoint.X - elem.DesiredSize.Width / 2, finalSize.Height / 2 + childPoint.Y - elem.DesiredSize.Height / 2);
+                //Call Arrange method on the child element by giving the calculated point as the placementPoint.
+                elem.Arrange(new Rect(actualChildPoint.X, actualChildPoint.Y, elem.DesiredSize.Width, elem.DesiredSize.Height));
+                //Calculate the new _angle for the next element
+                _angle += _incrementalAngularSpace;
+            }
+            return finalSize;
+        }
+    }
+}
---
+++ AppLib.WPF/Controls/Panels/WeightedPanel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AppLib.WPF.Controls
+{
+    /// <summary>
+    /// Weighted panel
+    /// </summary>
+    public class WeightedPanel: Panel
+    {
+        internal class ChildAndRect
+        {
+            public UIElement Element { get; set; }
+            public Rect Rectangle { get; set; }
+        }
+
+        private static FrameworkPropertyMetadata weightMetadata = 
+            new FrameworkPropertyMetadata(1.0, FrameworkPropertyMetadataOptions.AffectsParentArrange);
+
+        /// <summary>
+        /// Dependency property for setting item weight
+        /// </summary>
+        public static readonly DependencyProperty WeightProperty =
+            DependencyProperty.RegisterAttached("Weight", typeof(double), typeof(WeightedPanel), weightMetadata);
+
+        /// <summary>
+        /// setter for weight
+        /// </summary>
+        /// <param name="depObj"></param>
+        /// <param name="value"></param>
+        public static void SetWeight(DependencyObject depObj, double value)
+        {
+            depObj.SetValue(WeightProperty, value);
+        }
+
+        /// <inhreitdoc/>
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            double totalWeight = TotalChildWeight();
+
+            foreach (ChildAndRect child in ChildrenTreemapOrder(InternalChildren.Cast<UIElement>(), availableSize))
+                child.Element.Measure(child.Rectangle.Size);
+
+            return availableSize;
+        }
+
+        /// <inhreitdoc/>
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            foreach (ChildAndRect child in ChildrenTreemapOrder(InternalChildren.Cast<UIElement>(), finalSize))
+                child.Element.Arrange(child.Rectangle);
+
+            return finalSize;
+        }
+
+        private double TotalChildWeight()
+        {
+            double weightSum = 0;
+            foreach (UIElement elem in InternalChildren)
+                weightSum += (double)elem.GetValue(WeightProperty);
+
+            return weightSum;
+        }
+
+        /// <summary>
+        /// Return child elements orderd by weight (largest to
+        /// smallest), passing back Rect for each child
+        /// (size and location), implementing a (crude)
+        /// treemap.
+        /// </summary>
+        /// <param name="elems">Child elements to measure/arrange</param>
+        /// <param name="containerSize">Available container size</param>
+        /// <returns></returns>
+        private IEnumerable<ChildAndRect> ChildrenTreemapOrder(IEnumerable<UIElement> elems, Size containerSize)
+        {
+            double remainingWeight = TotalChildWeight();
+
+            double top = 0.0;
+            double left = 0.0;
+
+            // Alternate between left edge and top edge
+            bool leftEdge;
+
+            // Sort by weight
+            var childrenByWeight = elems.OrderByDescending(
+                e => (double)e.GetValue(WeightProperty));
+
+            // Allocate space for each child, one at a time.
+            // Moving left to right, top to bottom
+            foreach (var child in childrenByWeight)
+            {
+                leftEdge = (containerSize.Width - left) > (containerSize.Height - top);
+
+                Size size;
+
+                double childWeight = (double)child.GetValue(WeightProperty);
+                double pctArea = childWeight / remainingWeight;
+                remainingWeight -= childWeight;
+
+                // Entire height, proportionate width
+                if (leftEdge)
+                    size = new Size(pctArea * (containerSize.Width - left), containerSize.Height - top);
+
+                // Top edge - Entire width, proportionate height
+                else
+                    size = new Size(containerSize.Width - left, pctArea * (containerSize.Height - top));
+
+                yield return new ChildAndRect { Element = child, Rectangle = new Rect(new Point(left, top), size) };
+
+                if (leftEdge)
+                    left += size.Width;
+                else
+                    top += size.Height;
+            }
+        }
+    }
+}
