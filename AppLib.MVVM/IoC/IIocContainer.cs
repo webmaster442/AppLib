b50fb03b240c9ec1359b0ea077fc7d72c116e6c2
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-03-24 12:01:40 +0100

    Interface declaration expanded

--- AppLib.MVVM/IoC/IIocContainer.cs
+++ AppLib.MVVM/IoC/IIocContainer.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Reflection;
 
 namespace AppLib.MVVM.IoC
 {
     public interface IIocContainer
     {
         void Register<T>(Func<T> getter);
+        void RegisterCallingConstructor<TPublic, TImplementation>(ConstructorInfo constructor = null);
         bool Unregister<T>();
         T Resolve<T>();
         event EventHandler<ResolveGetEventArgs> ResolveGet;
