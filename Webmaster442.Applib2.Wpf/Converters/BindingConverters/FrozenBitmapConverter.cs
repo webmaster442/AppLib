e1fd9cf3de7bcd7fdf3a137e0b2df42a9d6423ea
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-11-27 20:03:21 +0100

    FrozenBitmapConverter added & some tweaks

---
+++ Webmaster442.Applib2.Wpf/Converters/BindingConverters/FrozenBitmapConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Webmaster442.Applib.Converters
+{
+    public class FrozenBitmapConverter : ConverterBase<FrozenBitmapConverter>, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Uri uri)
+                return BitmapHelper.CreateFrozenBitmap(uri);
+            else if (value is string str)
+                return BitmapHelper.CreateFrozenBitmap(str);
+            else
+                return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
