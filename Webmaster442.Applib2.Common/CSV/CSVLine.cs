7315a7b13e647da12743ab24de633b18f588d990
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-12-03 22:58:38 +0100

    Added CSV reader classes

---
+++ Webmaster442.Applib2.Common/CSV/CSVLine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Webmaster442.Applib.CSV
+{
+    public class CSVLine: IReadOnlyList<string>
+    {
+        private List<string> _row;
+
+        public CSVLine(string sourceLine, char delimiter) :base()
+        {
+            string[] parts = sourceLine.Split(delimiter);
+            _row = new List<string>(_row);
+        }
+
+        public string this[int index] => _row[index];
+
+        public int Count => _row.Count;
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return _row.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _row.GetEnumerator();
+        }
+    }
+}
---
+++ Webmaster442.Applib2.Common/CSV/CSVReader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Webmaster442.Applib.CSV
+{
+    public sealed class CSVReader: IDisposable, IEnumerable<CSVLine>
+    {
+        private TextReader _textReader;
+        private readonly char _delimiter;
+
+        public CSVReader(TextReader textReader, char delimiter = ';')
+        {
+            _textReader = textReader;
+            _delimiter = delimiter;
+        }
+
+        public void Dispose()
+        {
+            if (_textReader != null)
+            {
+                _textReader.Dispose();
+                _textReader = null;
+            }
+            GC.SuppressFinalize(this);
+        }
+
+        public IEnumerator<CSVLine> GetEnumerator()
+        {
+            string line;
+            while ((line = _textReader.ReadLine()) != null)
+            {
+                yield return new CSVLine(line, _delimiter);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
