a7354b0c122005ac378092503c169f7167adeaf7
Webmaster442 <ruzsinszki.gabor@gmail.com>
2018-12-08 22:28:12 +0100

    Added ini file parser

---
+++ Webmaster442.Applib2.Common/IniFile.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Webmaster442.Applib.PInvoke;
+
+namespace Webmaster442.Applib
+{
+    /// <summary>
+    /// An INI file manipulation program
+    /// </summary>
+    public sealed class IniFile
+    {
+        /// <summary>
+        /// Currently opened ini path
+        /// </summary>
+        public string Path { get; private set; }
+
+        /// <summary>
+        /// Creates a new Instance of IniFile
+        /// </summary>
+        /// <param name="path">Ini file path. File must exist</param>
+        public IniFile(string path)
+        {
+            if (path == null)
+                throw new ArgumentNullException(nameof(path));
+
+            if (!File.Exists(path))
+                throw new FileNotFoundException(path);
+
+            Path = path;
+        }
+
+        /// <summary>
+        /// Write a value to Ini file
+        /// </summary>
+        /// <param name="section">Section</param>
+        /// <param name="key">Key</param>
+        /// <param name="value">value</param>
+        public void WriteValue(string section, string key, string value)
+        {
+            if (string.IsNullOrEmpty(section))
+                throw new ArgumentNullException(nameof(section));
+
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+
+            Kernel32.WritePrivateProfileString(section, key, value, Path);
+        }
+
+        /// <summary>
+        /// Write a value to Ini file
+        /// </summary>
+        /// <param name="section">Section</param>
+        /// <param name="key">Key</param>
+        /// <param name="value">value</param>
+        public void WriteValue(string section, string key, object value)
+        {
+            if (string.IsNullOrEmpty(section))
+                throw new ArgumentNullException(nameof(section));
+
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+
+            Kernel32.WritePrivateProfileString(section, key, value.ToString(), Path);
+        }
+
+        /// <summary>
+        /// Read value from ini file
+        /// </summary>
+        /// <param name="section">section</param>
+        /// <param name="key">key</param>
+        /// <returns>key value</returns>
+        public string ReadValue(string section, string key)
+        {
+            if (string.IsNullOrEmpty(section))
+                throw new ArgumentNullException(nameof(section));
+
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+
+            StringBuilder temp = new StringBuilder(1024);
+
+            int i = Kernel32.GetPrivateProfileString(section, key, "", temp,  1024, Path);
+
+            return temp.ToString();
+        }
+    }
+}
--- Webmaster442.Applib2.Common/PInvoke/Kernel32.cs
+++ Webmaster442.Applib2.Common/PInvoke/Kernel32.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Webmaster442.Applib.PInvoke
 {
@@ -11,5 +12,11 @@ namespace Webmaster442.Applib.PInvoke
         [DllImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool FreeLibrary(IntPtr hModule);
+
+        [DllImport("kernel32")]
+        public static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        [DllImport("kernel32")]
+        public static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
     }
 }
