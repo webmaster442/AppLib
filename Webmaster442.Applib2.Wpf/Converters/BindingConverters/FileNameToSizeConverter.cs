a53a78d419dcfb5c745bfd57b51c2d1d08712332
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2019-01-28 19:38:37 +0100

    FileNameToSizeConverter added

---
+++ Webmaster442.Applib2.Wpf/Converters/BindingConverters/FileNameToSizeConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Data;
+using Webmaster442.Applib.Internals;
+
+namespace Webmaster442.Applib.Converters
+{
+    /// <summary>
+    /// Converts a file name to file size
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(string))]
+    public class FileNameToSizeConverter : ConverterBase<FileNameToSizeConverter>, IValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string name = value as string;
+
+            if (string.IsNullOrEmpty(name))
+                return Binding.DoNothing;
+
+            if (File.Exists(name))
+            {
+                var fi = new FileInfo(name);
+                return FileSizeCalculator.Calculate(fi.Length);
+            }
+            else if (Directory.Exists(name))
+            {
+                return "Dir";
+            }
+            else if (Directory.GetLogicalDrives().Contains(name))
+            {
+                DriveInfo di = new DriveInfo(name);
+                if (di.IsReady)
+                    return FileSizeCalculator.Calculate(di.TotalSize);
+                else
+                    return FileSizeCalculator.Calculate(0);
+            }
+            else
+                return Binding.DoNothing;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ Webmaster442.Applib2.Wpf/Internals/FileSizeCalculator.cs
@@ -0,0 +1,42 @@
+﻿namespace Webmaster442.Applib.Internals
+{
+    internal static class FileSizeCalculator
+    {
+        /// <summary>
+        /// Calculate a file size in bytes to a human readable size
+        /// </summary>
+        /// <param name="value">long input value</param>
+        /// <returns>Human readable file size</returns>
+        public static string Calculate(long value)
+        {
+            double val = System.Convert.ToDouble(value);
+            string unit = "Byte";
+            if (val > 1125899906842624)
+            {
+                val /= 1125899906842624;
+                unit = "EiB";
+            }
+            else if (val > 1099511627776D)
+            {
+                val /= 1099511627776D;
+                unit = "TiB";
+            }
+            else if (val > 1073741824D)
+            {
+                val /= 1073741824D;
+                unit = "GiB";
+            }
+            else if (val > 1048576D)
+            {
+                val /= 1048576D;
+                unit = "MiB";
+            }
+            else if (val > 1024D)
+            {
+                val /= 1024D;
+                unit = "kiB";
+            }
+            return string.Format("{0:0.###} {1}", val, unit);
+        }
+    }
+}
